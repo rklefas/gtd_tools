d0c0b72577cc80cea31c72a8661b43f74656a3e2
Ryan Klefas <ryan.klefas@gmail.com>
2023-03-14 21:43:31 -0400

    Catch some exceptions, slow pace of screen output, improve screens text in many areas

--- dirsort.py
+++ dirsort.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import pathlib
+from time import sleep
 from PIL import Image
 
 
@@ -21,7 +22,8 @@ def pickfolder(starting):
 		picked = None
 		folders = 0
 		files = 0
-		lineitem("Folder " + str(folders), "..")
+		lineitem("Currently in", os.path.abspath(dircheck))
+		lineitem("Action " + str(folders), "..")
 
 		for file in glob.glob(dircheck+"/*"):
 		
@@ -30,12 +32,12 @@ def pickfolder(starting):
 
 			if os.path.isdir(file):
 				folders = folders + 1
-				lineitem("Folder " + str(folders), file.replace(dircheck, ""))
-		
-		lineitem("Currently in", os.path.abspath(dircheck))
+				lineitem("Action " + str(folders), file.replace(dircheck, ""))
 		
 		if files > 0:
-			lineitem("Folder 99", "Sort current folder with " + str(files) + " files")
+			lineitem("", " ")
+			lineitem("Action 99", "Sort " + str(files) + " files!")
+			lineitem("", " ")
 
 		if picked == None:
 		
@@ -44,13 +46,15 @@ def pickfolder(starting):
 			if tmp == '..':
 				picked = 0
 			else:
-				picked = int(tmp)
+				try:
+					picked = int(tmp)
+				except:
+					continue
 
 		folders = 0
 
 		if picked == 99:
 			dircheck = os.path.abspath(dircheck)
-			confirmation('You have chosen to sort: ' + dircheck)
 			return dircheck
 		elif picked == 0:
 			dircheck = os.path.abspath(dircheck+"/..")
@@ -81,7 +85,7 @@ def sortfolder(dircheck):
 #			lineitem("Folder", file)
 
 	
-	print("There are "+ str(files) + " files in " + dircheck)
+	lineitem("  Files", str(files))
 	
 	if (files > 0):
 		for file in glob.glob(dircheck+"/*"):	
@@ -106,12 +110,14 @@ def sortfile(dircheck, file):
 			print('Exception opening file')
 			
 	
-	print("  (a)ctionable this year")
-	print("  (s)omeday")
-	print("  (r)eference")
-	print("  (n)ot sure")
+	lineitem("File", file.replace(dircheck, ""))
+	lineitem("options", "exit, open")
+	lineitem("", "(a)ctionable this year")
+	lineitem("", "(s)omeday")
+	lineitem("", "(r)eference")
+	lineitem("", "(n)ot sure")
 
-	folder = input(file.replace(dircheck, "")+" >>> When actionable? [a s r n]")
+	folder = input("  >>> When actionable? [a s r n] ")
 	
 	if folder == 'exit':
 		return folder
@@ -119,15 +125,15 @@ def sortfile(dircheck, file):
 	if folder != '':
 		
 		if folder == 'a':
-			folder = 'actionable'
+			folder = 'is actionable'
 		elif folder == 's':
-			folder = 'someday'
+			folder = 'is someday'
 		elif folder == 'r':
-			folder = 'reference'
+			folder = 'is reference'
 		elif folder == 'n':
-			folder = 'not sure'
+			folder = 'is not sure'
 			
-		newpath = dircheck + '/is ' + folder
+		newpath = dircheck + '/' + folder
 		
 		if not os.path.exists(newpath):
 			os.makedirs(newpath)
@@ -137,8 +143,12 @@ def sortfile(dircheck, file):
 	return 'moved to ' + folder
 
 def lineitem(key, value):
-	key = key + ":"
+
+	if len(key) > 0:
+		key = key + ":"
+		
 	print(key.ljust(15, " ") + value)
+	sleep(0.05)
 
 
 def confirmation(message):
